2749fcbb8d76214ba2a6e2fc39047a7d9adbea3e
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-02-18 19:10:17 -0800

    loading state added error file created

---
+++ app/meals/error.js
@@ -0,0 +1,7 @@
+'use client';
+
+export default function Error({error}) {
+    return <main className="error">
+        <h1>Sorry for inconvenience an error has occured.</h1>
+    </main>
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
-import { getMeals } from "@/fetchFunctions/meal"; 
+import { getMeals } from "@/fetchFunctions/meal";
+import { Suspense } from "react";
 
-export default async function Meals() {
+async function MealLoad() {
   // diferent way of fetching data
-  const meals = await getMeals()
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function Meals() {
   return (
     <>
       <section className={classes.header}>
@@ -13,17 +18,15 @@ export default async function Meals() {
           Deliciuos meals, created
           <span className={classes.highlight}>by you</span>
         </h1>
-        <p>
-            Choose your favorite recipe and cook it yourself. 
-        </p>
+        <p>Choose your favorite recipe and cook it yourself.</p>
         <p className={classes.cta}>
-            <Link href="/meals/share">
-                Share Your Favorite Recipe
-            </Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </section>
       <main className={classes.main}>
-        <MealsGrid meals={meals}/>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <MealLoad />
+        </Suspense>
       </main>
     </>
   );
