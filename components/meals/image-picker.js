a3c7686edd2323480e31c6b2cc397ce0d762bf35
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-02-21 18:32:03 -0800

    form can now be submited

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,9 +1,11 @@
 'use client';
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
+   const [pickImage, setPickImage] = useState()
     // for creating a custome button
     const imageInput = useRef();
     
@@ -12,10 +14,28 @@ export default function ImagePicker({ label, name }) {
         imageInput.current.click();
     }
 
+    function handleImageChange(event){
+      const file = event.target.files[0];
+      if (!file){
+        setPickImage(null);
+        return; 
+      }
+      const fileReader = new FileReader(); //
+      fileReader.onload = () => {
+        setPickImage(fileReader.result);
+      }; 
+
+      fileReader.readAsDataURL(file);
+    }
+
   return (
     <div className={classes.picker}>
       <label html={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+            {!pickImage && <p>No image picked yet.</p>}
+            {pickImage && <Image src={pickImage} alt="The image selected by the user." fill />}
+        </div>
         <input
         className={classes.input} // makes the original button invisable
           type="file"
@@ -23,6 +43,8 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput} //for custome image
+          onChange={handleImageChange}
+          required
         />
         <button className={classes.button} type="button" onClick={handlePickClick}>
             Pick an Image
